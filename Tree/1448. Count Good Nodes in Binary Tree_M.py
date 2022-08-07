6d9e682f58129541bc7e28c35254cbb5139e1624
jellyfiona <105749844+jellyfiona@users.noreply.github.com>
2022-08-07 10:31:53 -0700

    Update 1448. Count Good Nodes in Binary Tree_M.py

--- Tree/1448. Count Good Nodes in Binary Tree_M.py
+++ Tree/1448. Count Good Nodes in Binary Tree_M.py
@@ -52,3 +52,36 @@ class Solution:
             
         preOrder(root)
         return self.cnt
+
+
+    def goodNodes_V1(self, root: TreeNode) -> int:
+        """
+        [notice] the same basic idea with last function, but easier maxv_in_path trackor.
+        time: 07/25/2022 3:50--3:59
+        [Approach] First we know for sure that we need traverse the tree once.
+        As we traverse the tree, we give each node a parameter store the maximum value in the path from root to itself.
+        Also, we keep a tracker of the cnt of good node.
+        when we visit a node, we compare the val of a node ,with the maximum value in the path.
+        If the node is good, cnt increases by one.
+        time_O(N) 
+        space_O(N) 
+        """
+        
+        if root == None:
+            return 0
+        
+        self.cnt = 0
+        root.maxv = root.val
+        
+        def preOrder(root):
+            if root.maxv <= root.val:
+                self.cnt += 1
+            if root.left:
+                root.left.maxv = max(root.left.val, root.maxv)
+                preOrder(root.left)
+            if root.right :
+                root.right.maxv = max(root.right.val, root.maxv)
+                preOrder(root.right)
+                
+        preOrder(root)
+        return self.cnt
